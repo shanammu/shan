3a7af90c346f79ff42f522a09fe37901539ef5c3
shanammu <bshanmathi96@gmail.com>
2017-04-24 14:28:35 +0530

    Create largest of 3.c

---
+++ largest of 3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+scanf("%d,%d,%d",&a,&b,&c);
+if((a>b)&&((a>c))
+{
+printf("%d is the largest number",&a);
+}
+else if((b>a)&&(b>c))
+{
+printf("%d is the largest number",&b);
+}
+else
+{
+printf("%d is the largest number",&c);
+}
+getch();
+}
