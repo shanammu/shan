87f60dc31d64c31f8c6172adec47d23cb4f7f9ba
shanammu <bshanmathi96@gmail.com>
2017-06-08 11:56:28 +0530

    Create sum of odd numbers and sum of natursl numbers.c

---
+++ sum of odd numbers and sum of natursl numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,d=0;
+clrscr();
+if(i<=15)
+{
+for(i=0;i<15;i++)
+{
+sum+=i;
+}
+}
+for(i=15;1<=45;i+=2)
+{
+d+=i;
+}
+printf("sum is:%d",sum);
+printf("sumof odd number is:%d",d);
+getch();
+}
