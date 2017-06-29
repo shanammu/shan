b03b392636f0c745f091431224a6ea31a7df3fc7
shanammu <bshanmathi96@gmail.com>
2017-06-29 06:02:25 -0700

    Create days.c

---
+++ days.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[100];
+clrscr();
+printf("enter the day");
+gets(s);
+if(s=="sunday")
+{
+printf("false");
+}
+else
+{
+printf("true");
+}
+getch();
+}
